38c3cab5508b80d6b6a25b7dbc47d65f4ba4b957
Boris Belyaev <bbor1974@gmail.com>
2026-06-27 17:35:13 +0400

    BoardConfig: change version

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -73,7 +73,7 @@ BOARD_HAS_NO_SELECT_BUTTON := true
 TW_BRIGHTNESS_PATH := /sys/class/disp/disp/attr/lcd_bl
 TW_MAX_BRIGHTNESS := 255
 TW_DEFAULT_LANGUAGE := ru
-TW_DEVICE_VERSION := by bbor74 (nk)
+TW_DEVICE_VERSION := by bbor74 (nk)-7.1
 
 # TWRP_INCLUDE_LOGCAT := true
 # TARGET_USES_LOGD := true
